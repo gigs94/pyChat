5711c428e75333a89fcc9f5ac390ca5ff44eeddb
daniel gregory <gigs@dreadosx.attlocal.net>
2015-01-26 11:18:13 -0500

    basic layout of the client started

--- src/pyChat_client.py
+++ src/pyChat_client.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
-# -*- coding: iso-8859-1 -*-
+
+#-------------------------------------------------------------------------------
+__author__     = "Daniel Gregory"
+__email__      = "gigs94@gmail.com"
+__version__    = "0.0.0"
+__status__     = "Experimental"
+#-------------------------------------------------------------------------------
+
 
 import Tkinter
+import argparse
+import logging
+import json
+import os
+import pika
+from ConfigParser import RawConfigParser
 
-class simpleapp_tk(Tkinter.Tk):
+#-------------------------------------------------------------------------------
+class pychat_client(Tkinter.Tk):
     def __init__(self,parent):
         Tkinter.Tk.__init__(self,parent)
         self.parent = parent
@@ -45,7 +59,44 @@ class simpleapp_tk(Tkinter.Tk):
         self.entry.focus_set()
         self.entry.selection_range(0, Tkinter.END)
 
-if __name__ == "__main__":
-    app = simpleapp_tk(None)
-    app.title('my application')
+
+def get_users():
+    pass
+
+def create_account():
+    pass
+
+def send_msg():
+    pass
+
+def encrypt_msg():
+    pass
+
+def decrypt_msg():
+    pass
+
+def read_config_file():
+    pass
+
+def write_config_file():
+    pass
+
+
+
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='pyChat Client')
+    subparsers = parser.add_subparsers()
+
+    debug_parser = subparsers.add_parser('debug', help='debug help')
+    debug_parser.add_argument('directory')
+    debug_parser.set_defaults(func=debug)
+
+    args = parser.parse_args()
+    args.func(args)
+
+    app = pychat_client(None)
+    app.title('pyChat')
     app.mainloop()
+
+    sys.exit()
